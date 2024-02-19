7913c3e660060b9d9f4383aa738f9e29ef48846f
eflaatten <elliotflaatten@live.com>
2024-02-18 22:09:29 -0600

    evenodd list

---
+++ script.js
@@ -0,0 +1,22 @@
+let usersWord = ''
+let oddList = document.getElementById('odd')
+let evenList = document.getElementById('even')
+
+const updateWord = (element) => {
+  usersWord = element.value
+
+  console.log(usersWord)
+}
+
+const handleSubmit = () => {
+  let newListItem = document.createElement('LI')
+  newListItem.innerText = usersWord
+
+  if (usersWord.length % 2 === 0) {
+    evenList.appendChild(newListItem)
+  } else {
+    oddList.appendChild(newListItem)
+  }
+  usersWord = ""
+  document.getElementById("even-odd-form").reset()
+}
